10ad39336597a80f62e271df33c57dc16cc492d4
Ruikang (Leo) Guo <37886365+LeoKwo@users.noreply.github.com>
2025-06-13 10:00:13 -0400

    Update embed.py

--- few_shot/embed.py
+++ few_shot/embed.py
@@ -3,12 +3,22 @@ import requests
 from langchain_core.documents import Document
 from langchain_ollama.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
-from ..settings import getEMBED
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from settings import getEMBED
 
 embedding = getEMBED()
 
 docs = []
 with open("soup.jsonl", "r", encoding="utf-8") as f:
+    for line in f:
+        data = json.loads(line)
+        content = f"{data['soup']}\n\n故事真相:\n{data['story']}"
+        metadata = {}
+        docs.append(Document(page_content=content, metadata=metadata))
+
+with open("soup2.jsonl", "r", encoding="utf-8") as f:
     for line in f:
         data = json.loads(line)
         content = f"{data['soup']}\n\n故事真相:\n{data['story']}"
